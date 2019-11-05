223a3e366891632f3fe3013b5b5ef219357e8d48
Kirill Sinitsyn <k.s.sinitsyn@gmail.com>
2019-11-05 22:11:48 +0300

    Все пункты задания выполнены eslint проверен, все пашет, протестил в postman

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,46 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка при создании пользователя -- ${err}` }));
+// eslint-disable-next-line consistent-return
+module.exports.createUser = (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send({ message: 'Запрос пустой' });
+  }
+
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
+    .then((user) => res.status(201).send({
+      _id: user._id, name: user.name, about: user.about, email: user.email,
+    }))
+    .catch((err) => res.status(400).send({ message: `Ошибка при создании пользователя - Error: ${err}` }));
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        process.env.SECRET_KEY,
+        { expiresIn: '7d' },
+      );
+      res.status(201).cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
+      }).send({ message: 'Доступ открыт' });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
 };
 
 module.exports.getAllUsers = (req, res) => {
--- models/user.js
+++ models/user.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
-const validate = require('mongoose-validator');
-
-const urlValidate = validate({
-  validator: 'matches',
-  arguments: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/,
-});
+const validate = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,8 +18,44 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
-    validate: urlValidate,
+    validate: {
+      validator: (agr) => validate.isURL(agr),
+      message: 'Неверный формат url',
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (agr) => validate.isEmail(agr),
+      message: 'Неверный формат email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
   },
 });
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
